615518e7170d646ea10c2ec25b20474d9c92b1a5
user <kangse2942@gmail.com>
2017-07-19 16:28:58 +0900

    kangsoyee

--- app/src/main/java/example/com/smu_3_demo/AddressActivity.java
+++ app/src/main/java/example/com/smu_3_demo/AddressActivity.java
@@ -3,11 +3,11 @@ package example.com.smu_3_demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class AddressActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_address);
     }
 }
--- app/src/main/java/example/com/smu_3_demo/MainActivity.java
+++ app/src/main/java/example/com/smu_3_demo/MainActivity.java
@@ -6,13 +6,36 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] myStringArray={"사과","배","딸기","포도","복숭아"};
+
+    private AdapterView.OnItemClickListener mMessageClickedHandler=new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            String strText = (String) parent.getItemAtPosition(position);
+            Toast.makeText(getApplicationContext(), strText, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
+        ListView listview = (ListView) findViewById(R.id.listview);
+        listview.setAdapter((ListAdapter) adapter);
+        listView.setOnItemClickListener(mMessageClickedHandler);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -26,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent;
         switch (item.getItemId()) {
             case R.id.menu:
-                intent = new Intent(this, Main2Activity.class);
+                intent = new Intent(this, AddressActivity.class);
                 startActivity(intent);
                 return true;
             default:
