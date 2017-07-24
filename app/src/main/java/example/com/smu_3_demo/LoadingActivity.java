c2332ef92ba23d73bfb3cf9eaaef137d4e125121
Woojin1186 <jfseo2638@gmail.com>
2017-07-24 11:42:26 +0900

    색 변경 군종 선택 추가 로딩이미지 추가

--- app/src/main/java/example/com/smu_3_demo/LoadingActivity.java
+++ app/src/main/java/example/com/smu_3_demo/LoadingActivity.java
@@ -1,5 +1,7 @@
 package example.com.smu_3_demo;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);  // 로딩화면 시간
     }
 }
