b7a58f107ac54499a6947f17e72dbe9cee1a55a3
Woojin1186 <jfseo2638@gmail.com>
2017-07-24 15:29:16 +0900

    색 변경 군종 선택 추가 로딩이미지 추가 메인2 배경이미지 추가 메인2 디자인 변경 메인2 타이틀바 제거

--- app/src/main/java/example/com/smu_3_demo/Main2Activity.java
+++ app/src/main/java/example/com/smu_3_demo/Main2Activity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +24,8 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main2);
 
         final EditText nameInput = (EditText) findViewById(R.id.name);
